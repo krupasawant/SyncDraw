7e05700a98749db274eb2cfc37cd964c4276a38f
krupasawant <krupasawant05@gmail.com>
2025-09-25 22:27:50 +0530

    feat(auth): integrate Clerk authentication and signup/login flow

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,54 @@
+import './App.css';
+import { useState } from 'react';
 
-import './App.css'
-import  CanvasEditor  from './components/CanvasEditor'
-import Toolbar from './components/Toolbar'
-import EditPanel from './components/EditPanel'
+import CanvasEditor from './components/CanvasEditor';
+import Toolbar from './components/Toolbar';
+import EditPanel from './components/EditPanel';
+import Sidebar from './components/SideBar';
+import { ClerkProvider, SignedIn, SignedOut, RedirectToSignIn } from '@clerk/clerk-react';
 
+
+import type { BaseObject } from './types/object';
+import Navbar from './components/Navbar';
+const publishableKey =  import.meta.env.VITE_CLERK_FRONTEND_API;
 
 function App() {
   
+  const [selectedDesign, setSelectedDesign] = useState<{
+    _id?: string;
+    title?: string;
+    data: BaseObject[];
+  } | null>(null);
 
   return (
-    <div>
-      <Toolbar></Toolbar>
-      <CanvasEditor></CanvasEditor>
-       <EditPanel />
-    </div>
+    <ClerkProvider publishableKey={publishableKey}>
+       <Navbar></Navbar>
+    <SignedIn>
     
-  )
+    <div style={{ display: 'flex', height: '100vh' }}>
+      {/* Sidebar to load past designs */}
+     
+      <Sidebar
+        
+        onSelectDesign={(data, id, title) =>
+          setSelectedDesign({ _id: id, title, data })
+        }
+      />
+
+
+      {/* Main canvas and controls */}
+      <div style={{ flex: 1, position: 'relative' }}>
+        <Toolbar />
+        <CanvasEditor selectedDesign={selectedDesign || undefined} />
+        <EditPanel />
+      </div>
+    </div>
+    </SignedIn>
+    <SignedOut>
+        <RedirectToSignIn />
+      </SignedOut>
+    </ClerkProvider>
+  );
 }
 
-export default App
+export default App;
